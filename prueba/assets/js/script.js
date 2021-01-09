b6d712ea23ffd20a80585d14128205409db7dee4
jorge <jorgerioschile@gmail.com>
2021-01-09 02:28:56 -0600

    arreglo grillas seccion mis trabajos

--- prueba/assets/js/script.js
+++ prueba/assets/js/script.js
@@ -1,6 +1,21 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
-$("#email").click(function(){
-     alert("el correo fue enviado correctamente..");
-})
+$("a").click(function(event){
+     if (this.hash !=="");{
+         event.preventdefault();
+
+         var gato = this.hash;
+         $("html, body").animate({
+             scrolltop: $(gato).offset().top 
+             }, 900, function(){
+                 window.location.hash = gato;
+             });
+     }
+    });
+
+    $('nav a').click(function(e){
+        e.preventDefault();
+        $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
+    });
+    
