d8531c70ea96294b33664a43d1450b206e21eebf
jorge <jorgerioschile@gmail.com>
2021-01-09 23:05:22 -0600

    popovers seccion mis  trabajos

--- prueba/assets/js/script.js
+++ prueba/assets/js/script.js
@@ -1,21 +1,15 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-
-$("a").click(function(event){
-     if (this.hash !=="");{
-         event.preventdefault();
-
-         var gato = this.hash;
-         $("html, body").animate({
-             scrolltop: $(gato).offset().top 
-             }, 900, function(){
-                 window.location.hash = gato;
-             });
-     }
-    });
-
-    $('nav a').click(function(e){
-        e.preventDefault();
-        $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
-    });
-    
+$("about").click(function(event){
+    if (this.hash !=="");{
+        event.preventdefault();
+        var gato = this.hash;
+        $("html, body").animate({
+            scrolltop: $(gato).offset().top 
+            }, 900, function(){
+                window.location.hash = gato;
+            });
+    }
+   });
+   
+   $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
